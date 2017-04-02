e874bf844c0e30db4904c006d450ad06c812a984
MichaelHBBrooks <Michael.H.B.Brooks@gmail.com>
2017-04-01 23:33:19 -0400

    Setup for question.

---
+++ Kapitel Fünf-3/src/main.cpp
@@ -0,0 +1,7 @@
+/**
+ * Given a positive integer, print the next smallest and the next largest number that have the same
+ * number of 1 bits in their binary representation.
+ *
+ * Programmer response:
+ * I am delaying this section in favor of other, more likely questions.
+ */
